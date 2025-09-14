e6883bb0123e3f4ff3ce576eb29f801211de1ed9
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-14 19:58:25 +0300

    FieldFlower updated and small changes about other files

--- src/main/java/com/flowershop/model/flower/DecorativeFlower.java
+++ src/main/java/com/flowershop/model/flower/DecorativeFlower.java
@@ -1,8 +1,9 @@
 package com.flowershop.model.flower;
 
 /*
-* decorative type from class Flower (tulip, rose etc.)
-* */
+decorative type from class Flower (tulip, rose etc.)
+no additional info
+*/
 
 public class DecorativeFlower extends Flower {
     public DecorativeFlower(String name, double price, int stemLength, int freshnessLevel) {
--- src/main/java/com/flowershop/model/flower/FieldFlower.java
+++ src/main/java/com/flowershop/model/flower/FieldFlower.java
@@ -1,4 +1,24 @@
 package com.flowershop.model.flower;
 
-public class FieldFlower {
+/*
+field type from class Flower (sunflower, lavender etc.)
+additional info: flowering season
+ */
+
+public class FieldFlower extends Flower {
+    private String bloomingSeason; // like 'Spring' or 'Summer'
+
+    public FieldFlower (String name, double price, int stemLength, int freshnessLevel, String bloomingSeason) {
+        super(name, price, stemLength, freshnessLevel);
+        this.bloomingSeason = bloomingSeason;
+    }
+
+    public String getBloomingSeason() {
+        return bloomingSeason;
+    }
+
+    @Override
+    public String getFlowerType(){
+        return "FieldFlower (season: " + bloomingSeason + ")";
+    }
 }
