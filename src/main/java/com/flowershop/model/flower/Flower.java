14114f2e460948448729ddf0f0c6570abf60b27d
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-14 18:51:08 +0300

    created basic of abstract class Flower

--- src/main/java/com/flowershop/model/flower/Flower.java
+++ src/main/java/com/flowershop/model/flower/Flower.java
@@ -1,4 +1,25 @@
 package com.flowershop.model.flower;
 
-public class Flower {
+public abstract class Flower {
+    private String name;
+    private double price;
+    private int stemLength;
+    private int freshnessLevel; //1-dead, 10-fresh
+
+    public Flower(String name, double price, int stemLength, int freshnessLevel) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        if (stemLength <= 0) {
+            throw new IllegalArgumentException("Stem length should be positive");
+        }
+        if (freshnessLevel < 1 || freshnessLevel > 10) {
+            throw new IllegalArgumentException("Freshness level should be between 1 and 10");
+        }
+
+        this.name = name;
+        this.price = price;
+        this.stemLength = stemLength;
+        this.freshnessLevel = freshnessLevel;
+    }
 }
