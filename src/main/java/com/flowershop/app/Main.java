bf23186f21c5154bd509c2adce76a71c78bd7432
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-17 21:43:53 +0300

    imported our components for Main.java

--- src/main/java/com/flowershop/app/Main.java
+++ src/main/java/com/flowershop/app/Main.java
@@ -1,17 +1,11 @@
 package com.flowershop.app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import com.flowershop.model.Bouquet;
+import com.flowershop.model.Accessory;
+import com.flowershop.model.flower.FieldFlower;
+import com.flowershop.model.flower.DecorativeFlower;
+import com.flowershop.service.BouquetService;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+public class Main {
+
 }
