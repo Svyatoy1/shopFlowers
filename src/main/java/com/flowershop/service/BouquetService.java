03b1f6dc3f77391f493e76da7dd1cefe6ad227d5
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-17 20:20:14 +0300

    import libraries in BouquetService.java

--- src/main/java/com/flowershop/service/BouquetService.java
+++ src/main/java/com/flowershop/service/BouquetService.java
@@ -1,4 +1,11 @@
 package com.flowershop.service;
 
+import com.flowershop.model.Bouquet;
+import com.flowershop.model.flower.Flower;
+
+import java.util.Comparator; // interface for comparing objects
+import java.util.List; // interface for saving lists
+import java.util.stream.Collectors; // for saving results in collection
+
 public class BouquetService {
 }
