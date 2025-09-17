1a6fad2a00375bc3ad755400dac73b79c24c9475
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-17 20:30:36 +0300

    created empty methods for BouquetService.java

--- src/main/java/com/flowershop/service/BouquetService.java
+++ src/main/java/com/flowershop/service/BouquetService.java
@@ -15,5 +15,19 @@ import java.util.List; // interface for saving lists
 import java.util.stream.Collectors; // for saving results in collection
 
 public class BouquetService {
+    public double getPrice(Bouquet bouquet) {
+        // to do
+        return 0.0; // temporary
+    }
+
+    public List<Flower> sortFlowersByFreshness(Bouquet bouquet) {
+        // to do
+        return List.of(); // temporary
+    }
+
+    public List<Flower> findFlowersByStemLength(Bouquet bouquet) {
+        // to do
+        return List.of();
+    }
 
 }
