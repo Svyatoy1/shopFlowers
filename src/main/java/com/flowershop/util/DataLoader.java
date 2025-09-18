f8f00c00174212f07f254681b247828802d5fcba
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-18 17:14:26 +0300

    updated DataLoader.java as preparing for unit test

--- src/main/java/com/flowershop/util/DataLoader.java
+++ src/main/java/com/flowershop/util/DataLoader.java
@@ -1,4 +1,28 @@
 package com.flowershop.util;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+// utilite class for downloading data from resource files
+
 public class DataLoader {
+    public static List<String> loadLines(String fileName) {
+        List<String> lines = new ArrayList<>();
+
+        try (InputStream is = DataLoader.class.getClassLoader().getResourceAsStream(fileName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+
+            String line;
+            while ((line = reader.readLine()) != null){
+                lines.add(line.trim());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("The file could not be downloaded: " + fileName, e);
+        }
+
+        return lines;
+    }
 }
