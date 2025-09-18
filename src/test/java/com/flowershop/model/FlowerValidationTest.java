ef55a7a5eae351564fc95a34bcc194d9a4489a0b
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-09-18 16:23:45 +0300

    created unit test creating decorative flower

--- src/test/java/com/flowershop/model/FlowerValidationTest.java
+++ src/test/java/com/flowershop/model/FlowerValidationTest.java
@@ -1,4 +1,19 @@
 package com.flowershop.model;
 
-public class FlowerValidationTest {
+import com.flowershop.model.flower.Flower;
+import com.flowershop.model.flower.DecorativeFlower;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlowerValidationTest {
+    @Test
+    void testCreateDecorativeFlower() {
+        Flower rose = new DecorativeFlower("Rose", 50.0, 45,5);
+
+        assertEquals("Rose", rose.getName());
+        assertEquals(50.0, rose.getPrice());
+        assertEquals(45, rose.getStemLength());
+        assertEquals(5, rose.getFreshnessLevel());
+    }
 }
