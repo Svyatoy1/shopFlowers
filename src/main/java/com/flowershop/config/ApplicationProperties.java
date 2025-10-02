1e6d6f9d9b377fee5b6b4f4db3a476515e556490
Svyatoslav Pershko <svyatoslavpershko@gmail.com>
2025-10-02 15:39:42 +0300

    updated ApplicationProperties and pom.xml to connect with postgres

--- src/main/java/com/flowershop/config/ApplicationProperties.java
+++ src/main/java/com/flowershop/config/ApplicationProperties.java
@@ -1,4 +1,7 @@
 package com.flowershop.config;
 
 public class ApplicationProperties {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/shopflowers";
+    public static final String DB_USER = "postgres";
+    public static final String DB_PASSWORD = "12345678";
 }
